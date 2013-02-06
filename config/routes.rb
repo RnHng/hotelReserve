e7cab288a3608a0229eda89d0fce6da176c8405c
Geoffrey Cullins <gcullins@gmail.com>
2013-02-05 17:00:36 -0700

    Working through more layout stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 HotelReserve::Application.routes.draw do
-  get "static_pages/home"
+  get "reservations/new"
+
+  root to: 'static_pages#home'
+
+  match '/about',   to: 'static_pages#about'
+  match '/help',    to: 'static_pages#help'
+  match '/reserve', to: 'reservations#new'
+  match '/list',    to: 'reservations#list'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
